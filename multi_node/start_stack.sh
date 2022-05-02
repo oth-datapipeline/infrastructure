0d836a25c4731df74b8f36e48815c9b4e1660d6a
tharealbasti <sebastian.strasser98@web.de>
2022-05-02 13:03:38 +0200

    Remove options from compose which are not compatible with Docker Swarm

--- multi_node/start_stack.sh
+++ multi_node/start_stack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker network inspect swarm_network >/dev/null || docker network create --attachable --driver overlay swarm_network
-docker stack deploy -c docker-compose-kafka.yml news_pipeline_stack
+docker stack deploy -c docker-compose.yml news_pipeline_stack
 docker service scale news_pipeline_stack_kafka=3
